0e8246ea8136d76ffaf9eb889a744c4c23fa0376
kazuki.ogata <k-ogata@visor.co.jp>
2021-06-07 13:28:59 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by KAZUKI-OGATA on 2021/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
